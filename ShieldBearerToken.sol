6ca05ecd490a69d2585a655bef89991455ad6529
subratManish <154910483+subratManish@users.noreply.github.com>
2023-12-27 20:12:47 +0530

    Update ShieldBearerToken.sol

--- ShieldBearerToken.sol
+++ ShieldBearerToken.sol
@@ -1,1 +1,31 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ShieldBearerToken is ERC20, Ownable(msg.sender)  {
+    mapping(address => bool) private rewardsClaimed;
+    uint public constant initialSupply = 1000;
+
+    constructor() ERC20("ShieldBearerToken", "SBT") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mintTokens(address account, uint amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function claimReward() public {
+        require(balanceOf(msg.sender) >= initialSupply, "Insufficient balance");
+        require(!rewardsClaimed[msg.sender], "Rewards already claimed");
+
+        rewardsClaimed[msg.sender] = true;
+        _burn(msg.sender, initialSupply);
+    }
+
+    function annihilate(uint amount) public {
+        _burn(msg.sender, amount);
+    }
+}
